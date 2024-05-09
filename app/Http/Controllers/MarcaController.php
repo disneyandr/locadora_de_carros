33cee3479e3359c6eb77c56ac1da49dffb97a355
Disney <disneyandrade@gmail.com>
2024-05-09 14:41:54 -0300

    first commit

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marca;
+use Illuminate\Http\Request;
+
+class MarcaController extends Controller
+{
+    public function __construct(Marca $marca){
+        $this->marca = $marca;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        // $marcas = Marca::all();
+        $marcas = $this->marca->all();
+        return response()->json($marcas,200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        // $marca = Marca::create($request->all());
+        // dd($marca);
+
+        $request->validate($this->marca->regras(), $this->marca->feedback());
+        $marca = $this->marca->create($request->all());
+        return response()->json($marca,201);
+    }
+
+    /**
+     * Display the specified resource.
+     * @param Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $marca = $this->marca->find($id);
+        if($marca === null){
+            return response()->json(['erro' => 'Recurso pesquisado não existe'],404);
+        }
+        return response()->json($marca,200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Marca $marca)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $marca = $this->marca->find($id);
+        if($marca === null){
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe.'],404);
+        }
+       
+        if($request->method() === 'PATCH'){
+            $regrasDinamicas = array();
+            foreach($marca->regras() as $input => $regra){
+                if(array_key_exists($input,$request->all())){
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+            $request->validate($regrasDinamicas, $marca->feedback());
+        }else{
+
+            $request->validate($marca->regras(), $marca->feedback());
+        }
+        $marca->update($request->all());
+        return response()->json($marca,200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param Integer
+     * @param \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $marca = $this->marca->find($id);
+        if($marca === null){
+            return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe.'],404);
+        }
+        $marca->delete();
+        // print_r($marca->getAttributes()); //dados atuais
+        return response()->json(['msg'=>'Marca removida com sucesso!'],200);
+    }
+}
